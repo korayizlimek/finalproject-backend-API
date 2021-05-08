1fccd0b456fc5b48b33054461d7d647f70fb766e
Recai Koray izlimek <44255357+korayizlimek@users.noreply.github.com>
2021-05-08 11:42:26 +0300

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export function api() {
-    return axios.create({
-        baseUrl: "http://localhost:5000/api",
-    });
-}
-
-// api adresi degirisirse burayi da degistirecegiz
--- frontend/src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import "./Backdrop.css";
-function Backdrop({ show, click }) {
-    //arka fon // perde arkasi
-    return show && <div className="backdrop" onClick={click}></div>;
-}
-
-export default Backdrop;
